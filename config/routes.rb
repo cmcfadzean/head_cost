2c900e37f696d613f166a155357f105e1c95e2fe
cmcfadzean <court@kajabi.com>
2020-11-25 14:09:15 -0800

    adding new colors

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :employees
   resources :departments
   devise_for :users
-  root 'departments#index'
+  root 'employees#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
