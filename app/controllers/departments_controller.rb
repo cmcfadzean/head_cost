fef0531b75ca0d1f896e3d0ee81036c111156060
cmcfadzean <court@kajabi.com>
2020-12-01 22:42:05 -0800

    department page

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -15,6 +15,7 @@ class DepartmentsController < ApplicationController
   def show
     @departments = current_user.departments
     @employees = current_user.employees.where(:department => @department)
+    @employees_count = current_user.employees.where(:department => @department).count
     @total_cost = BigDecimal.new(0)
     @employees.each do |employee|
       unless employee.salaries.last.nil?
